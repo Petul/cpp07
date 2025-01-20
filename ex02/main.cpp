1de47751bb5435153644c696811730813055e753
Peter Leander <pleander@student.hive.fi>
2025-01-20 15:47:49 +0200

    Update tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,28 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <exception>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 #include "Array.hpp"
+#define HEADER(text)                                        \
+	std::cout << std::setfill('=') << std::setw(40) << "\n" \
+	          << text << "\n"                               \
+	          << std::setw(40) << "\n";
 
 int main(void)
 {
+	HEADER("Test empty int")
 	{
 		Array<int> a;
 		std::cout << a.size() << std::endl;
-		return (0);
 	}
+	HEADER("Test default int")
 	{
 		Array<int> a(5);
 		std::cout << a.size() << std::endl;
 		std::cout << a[0] << std::endl;
-		return (0);
 	}
+	HEADER("Test empty string")
 	{
 		Array<std::string> a(5);
 		std::cout << a.size() << std::endl;
 		std::cout << a[0] << std::endl;
-		return (0);
 	}
+	HEADER("Test out of bounds")
+	{
+		Array<int> a(5);
+		std::cout << a.size() << std::endl;
+		try
+		{
+			std::cout << a[5] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	return (0);
 }
